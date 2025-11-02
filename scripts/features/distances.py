627d7d4ab0aad346e54c7e74d55c910602c61f1e
Daniele Uras <daniele.uras@mail.polimi.it>
2025-11-02 13:00:53 +0100

    splitted the .ipynb into different .py

--- scripts/features/distances.py
+++ scripts/features/distances.py
@@ -32,9 +32,9 @@ import mne
 
 
 def geodesic_distance_matrix_from_ch_pos(
-    ch_pos: Dict[str, np.ndarray],
-    channel_order: List[str] = None,
-    radius: float = None
+    ch_pos: Dict[str, np.ndarray], 
+    channel_order: List[str] = None,  # type: ignore
+    radius: float = None # type: ignore
 ) -> Tuple[List[str], np.ndarray]:
     if channel_order is None:
         names = list(ch_pos.keys())
@@ -59,10 +59,8 @@ def geodesic_distance_matrix_from_ch_pos(
 
 
 def main():
-    # trova automaticamente la root del progetto
     project_root = Path(__file__).resolve().parents[2]
 
-    # costruisce il percorso corretto del file .locs
     locs_path = project_root / "scripts" / "data_processing" / "Preprocessing" / "ebneuro.locs"
 
     output_dir = project_root / "data" / "interim"
@@ -70,20 +68,20 @@ def main():
 
     montage = mne.channels.read_custom_montage(str(locs_path))
     ch_pos = montage.get_positions()['ch_pos']
-    ch_names = list(ch_pos.keys())
-    pos_array = np.array([ch_pos[ch][:3] for ch in ch_names])
+    ch_names = list(ch_pos.keys()) # type: ignore
+    pos_array = np.array([ch_pos[ch][:3] for ch in ch_names]) # type: ignore
 
     euclidean_distance = distance_matrix(pos_array, pos_array)
     df_euclidean = pd.DataFrame(euclidean_distance, index=ch_names, columns=ch_names)
     df_euclidean.to_csv(output_dir / "channel_pairwise_euclidean_distances.csv")
 
-    names, D_geo = geodesic_distance_matrix_from_ch_pos(ch_pos, channel_order=ch_names, radius=0.095)
+    names, D_geo = geodesic_distance_matrix_from_ch_pos(ch_pos, channel_order=ch_names, radius=0.095) # type: ignore
     df_geodesic = pd.DataFrame(D_geo, index=names, columns=names)
     df_geodesic.to_csv(output_dir / "channel_pairwise_geodesic_distances.csv")
 
     if 'Cz' in df_geodesic.index and 'T4' in df_geodesic.columns:
-        print("Geodesic distance Cz–T4:", float(df_geodesic.loc['Cz', 'T4']), "m\n")
-        print("Euclidean distance Cz–T4:", float(df_euclidean.loc['Cz', 'T4']), "m\n")
+        print("Geodesic distance Cz–T4:", float(df_geodesic.loc['Cz', 'T4']), "m\n") # type: ignore
+        print("Euclidean distance Cz–T4:", float(df_euclidean.loc['Cz', 'T4']), "m\n") # type: ignore
     else:
         print("Cz or T4 not found in channel names.")
 
